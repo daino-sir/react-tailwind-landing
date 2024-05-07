cd693845e4a2529ccbcd2490df091df99e0590bc
Daino Otieno <72624000+daino-sir@users.noreply.github.com>
2024-05-07 12:33:20 +0300

    added routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import { appRouter } from './Services/Navigation/AppRouter';
